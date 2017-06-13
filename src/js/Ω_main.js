a37a222b7ec35e1b6375349cc6cb98ea5867e511
thombanks <thomas.banks@wearecube3.com>
2017-06-13 15:34:42 +0100

    flex-testing new layout ideas

--- src/js/Ω_main.js
+++ src/js/Ω_main.js
@@ -1,27 +1,44 @@
 "use strict"
 
-window.onload = () => {
 
-    axios.get('data/data.json').then(function(response) {
-        const data = response.data
-        document.querySelector('main').innerHTML += "<br /><br /><strong>Adults</strong><br /><ul></ul>"
-        data.filter(isAdult).forEach((user) => {
-            document.querySelector('main ul').innerHTML += "<li>" + user.name + "</li>"
+// Log for debug
+console.log('js loaded', browser.width)
+const endpoint = "//scrummable.com/wp-json/wp/v2/posts?_embed"
+axios.get(endpoint).then((response) => {
+    response.data.forEach((article, index) => {
+        // log for debug
+        //console.info(article)
+        document.querySelector('.article-container-teaser').innerHTML += printArticle(article, index)
+    })
+
+    document.querySelectorAll('.article-hero-image-full').forEach((hero) => {
+        hero.addEventListener('load', () => {
+            document.querySelector('.article-hero-image-thumb').style.opacity = 0
+            setTimeout(() => {
+                document.querySelector('.article-hero-image-thumb').outerHTML = ''
+            }, 300)
+
         })
-
-        // Log for debug
-        console.log(data)
-
-    }).catch(function(error) {
-        // Log for debug
-        console.error(error)
     })
+}).catch((error) => {
+    console.error(error)
+})
 
-    document.querySelector('main').innerHTML += "<br />width: " + browser.width + "<br />height: " + browser.height
-
-
-    document.querySelector('#nudge').addEventListener('click', ()=>{
-      console.log(browser.height);
-      scrollTo(document.body, browser.height, 600)
-    })
+let printArticle = (article, index) => {
+    return `
+                <article class="article-teaser ${(index === 0) ? 'featured' : ''}">
+                    <header class="article-hero-wrapper" style="background-image: url('${article['_embedded']["wp:featuredmedia"][0].source_url}')">
+                        ${(index === 0) ? '<span class=\'featured-flag\'>featured</span>' : ''}
+                        <img class="article-hero-image-thumb" src="${article['_embedded']["wp:featuredmedia"][0].media_details.sizes.medium.source_url}" />
+                        <img class="article-hero-image-full" src="${article['_embedded']["wp:featuredmedia"][0].source_url}" />
+                    </header>
+                    <div class="content">
+                        <h2>${article.title.rendered}</h2>
+                        <p>${article.excerpt.rendered}</p>
+                    </div>
+                    <footer>
+                        <a href="" class="button">Read more</a>
+                    </footer>
+                </article>
+            `
 }
